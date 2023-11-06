a94fa9b232fb6fb808342f022296c4e91d1022a6
RosShelashskyi <ross.shelashskyi@tcu.edu>
2023-11-06 13:26:11 -0600

    Robustness added to addAPublihser

--- book_dao.py
+++ book_dao.py
@@ -1,4 +1,5 @@
 from mysql_connector import connection
+import mysql.connector
 
 def findAll():
     cursor = connection.cursor()
@@ -16,6 +17,11 @@ def addAPublisher(name, phone, city):
     cursor = connection.cursor()
     query = "insert into Publisher(name, phone, city) values (%s, %s, %s)"
     data = (name, phone, city)
-    cursor.execute(query, data)
-    connection.commit()
-    connection.close()
+    try:
+        cursor.execute(query, data)
+        connection.commit()
+    except mysql.connector.errors.IntegrityError:
+        cursor.close()
+        print("Duplicate entry: publisher <" + name + "> already exists")
+    cursor.close()
+    return "Publisher <" + name + "> added successfully"
